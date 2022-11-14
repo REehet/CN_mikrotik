39c624b62a8a7e7688207752f89fd0662bd1e909
REehet <mike-BV@users.noreply.github.com>
2022-11-14 19:26:18 +0800

    Create CN_Gen.py

---
+++ CN_Gen.py
@@ -0,0 +1,22 @@
+import requests
+
+url = "https://ispip.clang.cn/all_cn_cidr.txt"
+url2 = "https://ispip.clang.cn/all_cn_ipv6.txt"
+output = "/log info \"Loading CN address list\"\n/ip firewall address-list remove [/ip firewall address-list find list=CN]\n/ip firewall address-list\n"
+
+r = requests.get(url).text
+r2 = requests.get(url2).text
+
+lines = r.splitlines()
+for line in lines:
+    output += ":do { add address=" + line + " list=CN } on-error={}\n"
+
+lines2 = r2.splitlines()
+for line2 in lines2:
+    output += ":do { add address=" + line2 + " list=CN } on-error={}\n"
+
+with open('CN', 'w') as file:
+    file.write(output)
+
+with open('CN_backup', 'w') as file:
+    file.write(output.replace('CN', 'CN_backup'))
